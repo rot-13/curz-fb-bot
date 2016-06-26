559085a11cefad06ae24f7ade23b813175d85621
Ido Feins <ifeins@gmail.com>
2016-06-26 16:27:06 +0300

    added save video flow.

--- app/bot/app_server_client.rb
+++ app/bot/app_server_client.rb
@@ -1,10 +1,14 @@
 class AppServerClient
   include HTTParty
-  base_uri 'http://192.168.2.37:5000'
+  base_uri 'https://cpc-curz.herokuapp.com'
   debug_output $stdout
 
   def play_url(url)
     self.class.post('/play_url', body: {url: url})
+  end
+
+  def save_url(url, title)
+    self.class.post('/save_url', body: {url: url, title: title})
   end
 
 end
--- app/bot/example.rb
+++ app/bot/example.rb
@@ -7,33 +7,65 @@ end
 
 include Facebook::Messenger
 
+bot_sessions = {}
+
 Bot.on :message do |message|
   puts "Received #{message.text} from #{message.sender}"
 
-  # message.attachments[0]["payload"]["url"]
+  bot_session = bot_sessions[message.sender['id']]
+  if bot_session.try(:[], :state) == 'save_video_flow'
+    bot_session[:title] = message.text
+
+    # TODO: Save clip using AppServerClient
+
+    Bot.deliver(
+      recipient: message.sender,
+      message: {
+        text: "Saved video with title #{bot_session[:title]}"
+      }
+    )
+
+    hash.delete(message.sender['id'])
+    return
+  end
 
   if message.try(:attachments).try(:[], 0).try(:[], 'payload')
     url = message.attachments[0]['payload']['url']
-    AppServerClient.new.play_url(url)
-  end
-
-  Bot.deliver(
-      recipient: message.sender,
-      message: {
-          attachment: {
-              type: 'template',
-              payload: {
-                  template_type: 'button',
-                  text: 'Do you like to save it?',
-                  buttons: [
-                      {type: 'postback', title: 'Save', payload: 'SAVE_VIDEO'}
-                  ]
+    response = AppServerClient.new.play_url(url)
+    if response.code == 200 && response.parsed_response.starts_with?('playing')
+      Bot.deliver(
+          recipient: message.sender,
+          message: {
+              attachment: {
+                  type: 'template',
+                  payload: {
+                      template_type: 'button',
+                      text: 'Do you like to save it?',
+                      buttons: [
+                          {type: 'postback', title: 'Save', payload: {action: 'SAVE_CLIP', url: url}.to_json}
+                      ]
+                  }
               }
           }
-      }
-  )
+      )
+    end
+  end
+
 end
 
+Bot.on :postback do |postback|
+  payload = JSON.parse(postback.payload)
+
+  if payload['action'] == 'SAVE_CLIP'
+    bot_sessions[postback.sender['id']] = {state: 'save_video_flow', url: payload['url']}
+    Bot.deliver(
+       recipient: postback.sender,
+       message: {
+           text: 'Please enter a title for the clip'
+       }
+    )
+  end
+end
 
 
 Bot.on :delivery do |delivery|

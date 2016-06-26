9efeed3b9df11f9c6a8dd0dd4be3d935c91930b8
Ido Feins <ifeins@gmail.com>
2016-06-26 17:25:23 +0300

    updated scopes so had to generate a new page access token.

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -1,7 +1,7 @@
 require 'facebook/messenger'
 
 Facebook::Messenger.configure do |config|
-  config.access_token = 'EAALrnpYIsfYBABTgci00sInHqxB4KYbqCKwVM3546iEBnITSQFHZCXOYpuqfmGfg8IPC05Ph57DEuQJUbUXjQ47TLWK04pDponL1oYczZAGlowktjUiA8zYdUfMsoNsQH3Lwwq6oVs8p9cuiORwJEpMVBnxBHluKVgVdMtBAZDZD'
+  config.access_token = 'EAALrnpYIsfYBAE2ZBeJUh4MZBcLr8R8rxX7kvWif1eKNp4zYq0bwWJbjWDbOC7qbn3P1Q9CBrZAkBWLIXjBtqWBrPuBBDOl7l97gqQvHmkOcPEzCXAkAmf1uBrMvhwOqhMVGTN6vR3cqjlaZBPMQ6lbaB7DF71gX9t8CUzzVrwZDZD'
   config.verify_token = 'my_voice_is_my_password_verify_me'
 end
 
@@ -66,7 +66,6 @@ Bot.on :postback do |postback|
     )
   end
 end
-
 
 Bot.on :delivery do |delivery|
   puts "Delivered message(s) #{delivery.ids}"
